23ad10a96652ee9221e55fa49f2b3441769615fe
nettcod <nettcod@gmail.com>
2021-05-05 05:49:58 -0500

    Everything implemented and working except for saving camera frame buffer for telemetry feed

--- sources/cpp/monkey-vision/include/monkey_vision.h
+++ sources/cpp/monkey-vision/include/monkey_vision.h
@@ -9,51 +9,61 @@
  * provide a C-style wrapper for Rust interoperability.
  */
 #pragma once
+#include <stdbool.h>
+#include <sl/Camera.hpp>
+#include <opencv2/opencv.hpp>
+#include "aruco.hpp"
+
+//#define _DEBUG_
+
+/*** Aruco marker dictionary ***/
+const auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
+/*** ZED camera options ***/
+//VGA, HD720, HD1080, HD2K
+const sl::RESOLUTION ZedCameraResolution = sl::RESOLUTION::VGA;
+//VGA=100, HD720=60, HD1080=30, HD2K=15
+const int ZedCameraFps = 100;
+//PERFORMANCE, QUALITY, ULTRA 
+const sl::DEPTH_MODE ZedDepthQuality = sl::DEPTH_MODE::ULTRA;
+
+/*** ZED spatial mapping options ***/
+//MESH, FUSED_POINT_CLOUD
+const sl::SpatialMappingParameters::SPATIAL_MAP_TYPE ZedMapType = sl::SpatialMappingParameters::SPATIAL_MAP_TYPE::MESH;
+//LOW=8cm, MEDIUM=5cm, HIGH=2cm
+const float ZedMappingResolution = sl::SpatialMappingParameters::get(sl::SpatialMappingParameters::MAPPING_RANGE::MEDIUM);
+//NEAR=3.5m, MEDIUM=5m, FAR=10m
+const float ZedMappingRange = sl::SpatialMappingParameters::get(sl::SpatialMappingParameters::MAPPING_RANGE::MEDIUM);
+//LOW, MEDIUM, HIGH
+const sl::MeshFilterParameters::MESH_FILTER ZedMeshFilter = sl::MeshFilterParameters::MESH_FILTER::MEDIUM;
 
 /**
- * @brief C-style enum to wrap the RESOLUTION enum class used by the ZED library.
- */
-extern "C" enum ZedResolution
-{
-    Res2K, /**< Maps to RESOLUTION::HD2K in the ZED library and runs at 2K, 15fps*/
-    Res1080P, /**< Maps to RESOLUTION::HD1080 and runs at 1080p, 30fps */
-    Res720P, /**< Maps to RESOLUTION::HD720 and runs at 720p, 60fps */
-    ResUSB2 /**< Maps to RESOLUTION::VGA and runs at 672x376, 30fps; this is the only usable video mode for a ZED connected over a USB2.0 interface */
-};
-
-/**
- * @brief C-style enum to wrap the DEPTH_MODE enum class used by the ZED library.
- */
-extern "C" enum ZedDepthQuality
-{
-    PERFORMANCE_DEPTH, /**< Maps to DEPTH_MODE::PERFORMANCE */
-    QUALITY_DEPTH, /**< Maps to DEPTH_MODE:: */
-    ULTRA_DEPTH /**< Maps to DEPTH_MODE::ULTRA */
-};
-
-/**
- * @brief Structure to contain position data for an identified Aruco marker. All members default to -1 in case no Aruco marker was detected.
+ * @brief Structure to contain position data for an identified Aruco marker.
  */
 extern "C" struct ArucoData {
-    float distance = -1; /**< Stereo-measured scalar distance to the marker in meters */
-    float x_dist = -1; /**< Pose-estimated X-axis distance to marker in meters */
-    float y_dist = -1; /**< Pose-estimated Y-axis distance to marker in meters */
-    float z_dist = -1; /**< Pose-estimated Z-axis distance to marker in meters */
-    float x_rot = -1; /**< Pose-estimated X-axis rotation of the marker in degrees */
-    float y_rot = -1; /**< Pose-estimated Y-axis rotation of marker in degrees */
-    float z_rot = -1; /**< Pose-estimated Z-axis rotation of marker in degrees */
+    float x_dist = 0; /**< Pose-estimated X-axis distance to marker in meters */
+    float y_dist = 0; /**< Pose-estimated Y-axis distance to marker in meters */
+    float z_dist = 0; /**< Pose-estimated Z-axis distance to marker in meters */
+    float x_rot = 0; /**< Pose-estimated X-axis rotation of the marker in degrees */
+    float y_rot = 0; /**< Pose-estimated Y-axis rotation of marker in degrees */
+    float z_rot = 0; /**< Pose-estimated Z-axis rotation of marker in degrees */
 };
 
 /**
- * @brief Structure to contain acceleration and orientation data measured from the ZED IMU. All members default to -1 in case the IMU could not be accessed.
+ * @brief Structure to contain acceleration and orientation data measured from the ZED IMU.
  */
 extern "C" struct ZedImuData {
-    float x_accel = -1; /**< Linear acceleration of the ZED along the X-axis in m/s^2 */
-    float y_accel = -1; /**< Linear acceleration of the ZED along the Y-axis in m/s^2 */
-    float z_accel = -1; /**< Linear acceleration of the ZED along the Z-axis in m/s^2 */
-    float x_rot_accel = -1; /**< Angular acceleration of the ZED around the X-axis in deg/s */
-    float y_rot_accel = -1; /**< Angular accleration of the ZED around the Z-axis in deg/s */
-    float z_rot_accel = -1; /**< Angular acceleration of the ZED around the Z-axis in deg/s */
+    float x_accel = 0; /**< Linear acceleration of the ZED along the X-axis in m/s^2 */
+    float y_accel = 0; /**< Linear acceleration of the ZED along the Y-axis in m/s^2 */
+    float z_accel = 0; /**< Linear acceleration of the ZED along the Z-axis in m/s^2 */
+    float x_rot_vel = 0; /**< Angular velocity of the ZED around the X-axis in deg/s */
+    float y_rot_vel = 0; /**< Angular velocity of the ZED around the Z-axis in deg/s */
+    float z_rot_vel = 0; /**< Angular velocity of the ZED around the Z-axis in deg/s */
+    float x_pos = 0; /**< Estimated X-axis displacement of the ZED relative to the origin in meters */
+    float y_pos = 0; /**< Estimated Y-axis displacement of the ZED relative to the origin in meters */
+    float z_pos = 0; /**< Estimated Z-axis displacement of the ZED relative to the origin in meters */
+    float x_rot = 0; /**< X-axis orientation of the ZED in degrees */
+    float y_rot = 0; /**< Y-axis orientation of the ZED in degrees */
+    float z_rot = 0; /**< Z-axis orientation of the ZED in degrees */
 };
 
 /**
@@ -63,27 +73,18 @@ extern "C" struct FrameBuffer {
     uchar *buffer; /**< Byte array which stores the image frame */
     uint32_t width = 0; /**< Width of the image in pixels */
     uint32_t height = 0; /**< Height of the image in pixels */
-}
-
-/**
- * @brief Intialize camera
- * @param camera_res Enum to select the resolution at which the ZED camera should run.
- * @param depth Enum to select the depth mode which the ZED should use.
- *        NOTE: Higher quality depth measurement will use more GPU resources.
- * @return Boolean value indicating whether the ZED camera was successfully initialized.
- */
-extern "C" bool visual_processing_init(ZedResolution camera_res, ZedDepthQuality depth);
+};
 
 /**
  * @brief Get positional data for an Aruco marker encoding a given ID value.
  * @param aruco_id ID value encoded by a detected Aruco marker.
- * @return Structure containing position data for the Aruco marker with the given ID. Struct will have values of -1 if an Aruco marker with the given ID was not detected.
+ * @return Structure containing position data for the Aruco marker with the given ID.
  */
 extern "C" ArucoData get_aruco_data(int aruco_id);
 
 /**
  * @brief Get acceleration, orientation, and position data from the ZED IMU sensor.
- * @return Structure containing data from the ZED IMU. Struct will have values of -1 if the ZED IMU could not be accessed.
+ * @return Structure containing data from the ZED IMU.
  */
 extern "C" ZedImuData get_zed_imu_data();
 
@@ -94,13 +95,31 @@ extern "C" ZedImuData get_zed_imu_data();
 extern "C" FrameBuffer get_camera_frame();
 
 /**
+ * @brief Set a flag to update the map mesh file the next time run_visual_processing() is called.
+ *        NOTE: only update the mesh periodically as it is resource-intensive to do so.
+ */
+extern "C" void request_map_update();
+
+/**
+ * @brief Get the number of frames that have been processed.
+ * @return Integer count of the number of times run_visual_processing() has been called.
+ */
+extern "C" long get_frame_count();
+
+/**
+ * @brief Intialize camera
+ * @param mesh_path C-string path to save the stereo-scanned 3D environment mesh to.
+ * @return Boolean value indicating whether the ZED camera was successfully initialized.
+ */
+extern "C" bool visual_processing_init(const char *mesh_path);
+
+/**
  * @brief Run visual processing on a single video frame from the ZED camera.
  * @param marker_size Width of the Aruco marker in meters.
- * @param mesh_path C-string path to save the stereo-scanned 3D environment mesh to.
  * @param display Boolean value indicating if the function should display the camera view in a GUI window.
- * @return ArucoData struct for the aruco with the given ID. If no marker was detected with the given ID, return a struct with values of -1 for all parameters. 
+ * @return Boolean value indicating whether a camera frame could be successfully captured. 
  */
-extern "C" bool run_visual_processing(float marker_size, const char *mesh_path, bool display);
+extern "C" bool run_visual_processing(float marker_size, bool display);
 
 /**
  * @brief Deallocate all dynamic memory and close ZED camera.
--- sources/cpp/monkey-vision/src/monkey_vision.cpp
+++ sources/cpp/monkey-vision/src/monkey_vision.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-#include <opencv2/opencv.hpp>
-#include <sl/Camera.hpp>
-#include "aruco.hpp"
 #include "monkey_vision.h"
 
 // ZED camera handler
 sl::Camera zed;
+//ZED spatial mapping mesh
+const char *mesh_file_path;
+sl::Mesh map_mesh;
+// Frame counter for periodic tasks
+long frame_count;
 // Vectors for storing detected Aruco marker IDs and positional data
 std::vector<int> detected_ids;
 std::vector<ArucoData> detected_markers;
@@ -14,122 +16,12 @@ std::vector<ArucoData> detected_markers;
 ZedImuData zed_imu_data;
 // Struct for storing the latest camera capture
 FrameBuffer camera_frame;
-
-// Aruco dictionary used for detection - Aruco Original
-const auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
-
-// Get corresponding OpenCV matrix subtype for a given ZED library matrix subtype
-int getOCVtype(sl::MAT_TYPE type);
-// Convert ZED matrix object to OpenCV matrix object
-// TODO pls check is ambiguous
-cv::Mat slMat2cvMat(sl::Mat &input);
-// Get corresponding depth measurement at specified camera pixel coordinate
-float get_pixel_depth(int x, int y, sl::Mat img);
-
-int getOCVtype(sl::MAT_TYPE type)
-{
-    int cv_type = -1;
-    switch (type)
-    {
-        case sl::MAT_TYPE::F32_C1:
-            cv_type = CV_32FC1;
-            break;
-        case sl::MAT_TYPE::F32_C2:
-            cv_type = CV_32FC2;
-            break;
-        case sl::MAT_TYPE::F32_C3:
-            cv_type = CV_32FC3;
-            break;
-        case sl::MAT_TYPE::F32_C4:
-            cv_type = CV_32FC4;
-            break;
-        case sl::MAT_TYPE::U8_C1:
-            cv_type = CV_8UC1;
-            break;
-        case sl::MAT_TYPE::U8_C2:
-            cv_type = CV_8UC2;
-            break;
-        case sl::MAT_TYPE::U8_C3:
-            cv_type = CV_8UC3;
-            break;
-        case sl::MAT_TYPE::U8_C4:
-            cv_type = CV_8UC4;
-            break;
-        default:
-            break;
-    }
-    return cv_type;
-}
-
-cv::Mat slMat2cvMat(sl::Mat &input)
-{
-    // Since Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
-    // Mat and sl::Mat will share a single memory structure
-    return cv::Mat(input.getHeight(), input.getWidth(), getOCVtype(input.getDataType()), input.getPtr<sl::uchar1>(sl::MEM::CPU), input.getStepBytes(sl::MEM::CPU));
-}
-
-float get_pixel_depth(int x, int y, sl::Mat img)
-{
-    // Convert image pixel to 3d point from point cloud
-    sl::float4 point3d;
-    img.getValue(x, y, &point3d);
-    // Calculate absolute point depth from X,Y,Z
-    float depth = sqrt(point3d.x * point3d.x + point3d.y * point3d.y + point3d.z * point3d.z);
-    return depth;
-}
-
-bool visual_processing_init(ZedResolution camera_res, ZedDepthQuality depth)
-{
-    sl::InitParameters init_params;
-    // Select ZED camera resolution and FPS from argument
-    switch (camera_res)
-    {
-        case ZedResolution::Res2K:
-            init_params.camera_resolution = sl::RESOLUTION::HD2K;
-            init_params.camera_fps = 15;
-            break;
-        case ZedResolution::Res1080P:
-            init_params.camera_resolution = sl::RESOLUTION::HD1080;
-            init_params.camera_fps = 30;
-            break;
-        case ZedResolution::Res720P:
-            init_params.camera_resolution = sl::RESOLUTION::HD720;
-            init_params.camera_fps = 60;
-            break;
-        default:
-            init_params.camera_resolution = sl::RESOLUTION::VGA;
-            init_params.camera_fps = 30;
-    }
-    // Select ZED camera depth mode from argument
-    switch (depth)
-    {
-        case PERFORMANCE_DEPTH:
-            init_params.depth_mode = sl::DEPTH_MODE::PERFORMANCE;
-            break;
-        case QUALITY_DEPTH:
-            init_params.depth_mode = sl::DEPTH_MODE::QUALITY;
-            break;
-        case ULTRA_DEPTH:
-            init_params.depth_mode = sl::DEPTH_MODE::ULTRA;
-            break;
-        default:
-            init_params.depth_mode = sl::DEPTH_MODE::NONE;
-    }
-    init_params.coordinate_units = sl::UNIT::METER;
-    init_params.coordinate_system = sl::COORDINATE_SYSTEM::RIGHT_HANDED_Y_UP;
-    // Initialize camera using given parameters
-    auto returned_state = zed.open(init_params);
-    if (returned_state != sl::ERROR_CODE::SUCCESS)
-    {
-        std::cout << "Error " << returned_state << "." << std::endl;
-        return false;
-    }
-    std::cout << "Loaded dictionary: Aruco Original." << std::endl;
-    return true;
-}
+// Flag to update the map mesh while processing the next camera frame
+bool update_map_mesh = false;
 
 ArucoData get_aruco_data(int aruco_id)
 {
+    ArucoData return_data;
     // Check if any markers were detected
     if (detected_ids.size() > 0)
     {
@@ -139,12 +31,10 @@ ArucoData get_aruco_data(int aruco_id)
         {
             // Return data for the Aruco marker which encodes the given ID
             int index = iterator - detected_ids.begin();
-            return detected_markers[index];
+            return_data = detected_markers[index];
         }
     }
-    // If an Aruco with the given ID has not been detected, return a default ArucoData struct with -1 for all member values
-    ArucoData default_no_data;
-    return default_no_data;
+    return return_data;
 }
 
 ZedImuData get_zed_imu_data()
@@ -157,8 +47,59 @@ FrameBuffer get_camera_frame()
     return camera_frame;
 }
 
-bool run_visual_processing(float marker_size, const char *mesh_path, bool display)
+void request_map_update()
 {
+    update_map_mesh = true;
+}
+
+long get_frame_count()
+{
+    return frame_count;
+}
+
+bool visual_processing_init(const char *mesh_path)
+{
+    // Initialize empty map mesh
+    mesh_file_path = mesh_path;
+    map_mesh.save(mesh_file_path, sl::MESH_FILE_FORMAT::PLY);
+    //Set ZED camera parameters
+    sl::InitParameters init_params;
+    init_params.camera_resolution = ZedCameraResolution;
+    init_params.camera_fps = ZedCameraFps;
+    init_params.depth_mode = ZedDepthQuality;
+    init_params.coordinate_units = sl::UNIT::METER;
+    init_params.coordinate_system = sl::COORDINATE_SYSTEM::RIGHT_HANDED_Y_UP;
+    // Initialize camera using given parameters
+    auto returned_state = zed.open(init_params);
+    if (returned_state != sl::ERROR_CODE::SUCCESS)
+    {
+        std::cout << "Error " << returned_state << "." << std::endl;
+        return false;
+    }
+    // Enable IMU position tracking
+    sl::PositionalTrackingParameters tracking_params;
+    returned_state = zed.enablePositionalTracking(tracking_params);
+    if (returned_state != sl::ERROR_CODE::SUCCESS) 
+    {
+        return false;
+    }
+    // Enable spatial mapping
+    sl::SpatialMappingParameters mapping_params;
+    mapping_params.map_type = ZedMapType;
+    mapping_params.resolution_meter = ZedMappingResolution;
+    mapping_params.range_meter = ZedMappingRange;
+    returned_state = zed.enableSpatialMapping(mapping_params);
+    if (returned_state != sl::ERROR_CODE::SUCCESS) 
+    {
+        return false;
+    }
+    // Return true upon successful initialization
+    return true;
+}
+
+bool run_visual_processing(float marker_size, bool display)
+{
+    //Setup camera frame capture
     auto cameraInfo = zed.getCameraInformation();
     sl::Resolution image_size = cameraInfo.camera_resolution;
     sl::Mat image_zed(image_size, sl::MAT_TYPE::U8_C4);
@@ -174,25 +115,23 @@ bool run_visual_processing(float marker_size, const char *mesh_path, bool displa
 
     cv::Matx<float, 4, 1> dist_coeffs = cv::Vec4f::zeros();
 
-    sl::Pose zed_pose;
     std::vector<cv::Vec3d> rvecs, tvecs;
     std::vector<std::vector<cv::Point2f>> corners;
 
-    if (zed.grab() == sl::ERROR_CODE::SUCCESS)
+    // Capture a camera frame
+    if (zed.grab() == sl::ERROR_CODE::SUCCESS && zed.getSpatialMappingState() == sl::SPATIAL_MAPPING_STATE::OK)
     {
         // Reset data for new frame
         detected_ids.clear();
         detected_markers.clear();
-        zed_imu_data = {-1,-1,-1,-1,-1,-1};
+        zed_imu_data = {0,0,0,0,0,0};
 
         // Retrieve the left image
         zed.retrieveImage(image_zed, sl::VIEW::LEFT, sl::MEM::CPU, image_size);
-
         // Convert to RGB
         cvtColor(image_ocv, image_ocv_rgb, cv::COLOR_RGBA2RGB);
         // Detect markers
         cv::aruco::detectMarkers(image_ocv_rgb, dictionary, corners, detected_ids);
-
         // If at least one marker detected
         if (detected_ids.size() > 0)
         {
@@ -207,20 +146,56 @@ bool run_visual_processing(float marker_size, const char *mesh_path, bool displa
                 marker_data.y_dist = tvecs[index](1);
                 marker_data.z_dist = tvecs[index](2);
                 marker_data.x_rot = rvecs[index](0);
-                marker_data.x_rot = rvecs[index](1);
-                marker_data.x_rot = rvecs[index](2);
+                marker_data.y_rot = rvecs[index](1);
+                marker_data.z_rot = rvecs[index](2);
                 detected_markers.push_back(marker_data);
             }
             // Draw on image to highlight detected markers
             cv::aruco::drawDetectedMarkers(image_ocv_rgb, corners, detected_ids);
             cv::aruco::drawAxis(image_ocv_rgb, camera_matrix, dist_coeffs, rvecs[0], tvecs[0], marker_size * 1.5f);
         }
-
         // Display image
         if (display)
         {
-            imshow("Image", image_ocv_rgb);
+            imshow("ZED Camera Feed", image_ocv_rgb);
         }
+
+        // Read IMU data
+        sl::SensorsData sensor_data;
+        zed.getSensorsData(sensor_data, sl::TIME_REFERENCE::IMAGE);
+        sl::float3 linear_accel = sensor_data.imu.linear_acceleration;
+        sl::float3 angular_vel = sensor_data.imu.angular_velocity;
+        sl::float3 translation = sensor_data.imu.pose.getTranslation();
+        sl::float3 orientation = sensor_data.imu.pose.getOrientation();
+        // Store IMU data
+        zed_imu_data.x_accel = linear_accel[0];
+        zed_imu_data.y_accel = linear_accel[1];
+        zed_imu_data.z_accel = linear_accel[2];
+        zed_imu_data.x_rot_vel = angular_vel[0];
+        zed_imu_data.y_rot_vel = angular_vel[1];
+        zed_imu_data.z_rot_vel = angular_vel[2];
+        zed_imu_data.x_pos = translation[0];
+        zed_imu_data.y_pos = translation[1];
+        zed_imu_data.z_pos = translation[2];
+        zed_imu_data.x_rot = orientation[0];
+        zed_imu_data.y_rot = orientation[1];
+        zed_imu_data.z_rot = orientation[2];
+
+        // Update spatial map mesh every 30 frames (only done periodically because resource-intensive)
+        if (update_map_mesh)
+        {
+            zed.requestSpatialMapAsync();
+            update_map_mesh = false;
+        }
+        // Retrieve, filter, and save updated map when ready
+        if (zed.getSpatialMapRequestStatusAsync() == sl::ERROR_CODE::SUCCESS)
+        {
+            zed.retrieveSpatialMapAsync(map_mesh);
+            map_mesh.filter(ZedMeshFilter);
+            map_mesh.save(mesh_file_path, sl::MESH_FILE_FORMAT::PLY);
+        }
+
+        frame_count++;
         return true;
     }
     return false;
@@ -228,19 +203,33 @@ bool run_visual_processing(float marker_size, const char *mesh_path, bool displa
 
 void visual_processing_dealloc()
 {
+    zed.disableSpatialMapping();
+    zed.disablePositionalTracking();
     zed.close();
 }
 
 //Demo program
 int main(int argc, char **argv)
 {
-    visual_processing_init(ZedResolution::ResUSB2, ZedDepthQuality::ULTRA_DEPTH);
+    std::string path = "./mesh";
+    visual_processing_init(path.c_str()); 
+    std::cout << "Loaded dictionary: Aruco Original." << std::endl;
     // Loop until 'q' is pressed
     char key = '.';
     while (key != 'q')
     {
-        std::string path = "./mesh.obj";
-        run_visual_processing(0.10, path.c_str(), true);
+        if (frame_count % 60 == 0 && frame_count > 0)
+        {
+            request_map_update();
+            std::cout << "Frame #" << frame_count << ", updating mesh..." << std::endl;
+#ifdef _DEBUG_
+            ArucoData marker = get_aruco_data(69);
+            ZedImuData imu_data = get_zed_imu_data();
+            std::cout << "Aruco Marker 69: X=" << marker.x_dist << "m, Z=" << marker.z_dist << "m, Angle = " << marker.y_rot << "deg" << std::endl;
+            std::cout << "IMU: X=" << imu_data.x_rot << "deg, Y=" << imu_data.y_rot << "deg, Z=" << imu_data.z_rot << "deg" << std::endl;
+#endif 
+        }
+        run_visual_processing(0.10, true);
         key = cv::waitKey(10);
     }
     visual_processing_dealloc();
