f1bafd06f2baed70c97992d564b2d5a26eeb8e1a
Wallace Watler <watlerkw@msoe.edu>
2021-05-11 00:39:59 -0500

    Autonomous control loop is now threaded with channels.

--- sources/rust/monkey-api/src/objects/requests.rs
+++ sources/rust/monkey-api/src/objects/requests.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 #[derive(Debug, PartialOrd, PartialEq, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum LocationClass {
@@ -21,25 +23,15 @@ pub struct AutonomousParams {
     pub allow_backwards: bool,
     pub stopping_dist: f32,
     pub interaction_radius: f32,
+    pub min_x: f32,
+    pub max_x: f32,
+    pub min_z: f32,
+    pub max_z: f32,
+    pub res_x: u16,
+    pub res_z: u16,
+    pub min_mesh_to_grid_period: Duration,
     pub vertical_cutoff: f32,
     pub min_turn_radius: f32,
     pub drive_width: f32,
     pub wheel_radius: f32
 }
-
-// Parameters defining autonomous behavior
-// const MIN_SPATIAL_MAP_UPDATE_PERIOD: usize = 500;
-// const MAX_SPEED: f32 = 100.0;
-// const MAX_FORCE: f32 = 100.0;
-// const MASS: f32 = 1.0;
-// const MOMENT_OF_INERTIA: f32 = 1.0;
-// const ALLOW_BACKWARDS: bool = false;
-// const STOPPING_DIST: f32 = 100.0;
-// const INTERACTION_RADIUS: f32 = 0.2;
-// const MIN_X: f32 = -5.0;
-// const MAX_X: f32 = 1.0;
-// const MIN_Z: f32 = -7.0;
-// const MAX_Z: f32 = 0.0;
-// const RES_X: usize = 128;
-// const RES_Z: usize = 128;
-// const VERTICAL_CUTOFF: f32 = 1.0;
--- sources/rust/space-monkeys/src/lib.rs
+++ sources/rust/space-monkeys/src/lib.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::{thread, panic};
 
 use log::error;
 
@@ -8,11 +9,11 @@ use monkey_api::objects::requests::AutonomousParams;
 use monkey_pathfinding::a_star::AStar;
 use monkey_pathfinding::model::{DiscreteState, MonkeyModel, RobotVector};
 use monkey_pathfinding::state_space::MonkeyStateSpace;
-use monkey_vision::core::MonkeyVision;
 
 use crate::aimbot::{Path, Vehicle};
 use crate::math::Vec2D;
 use crate::mesh_to_grid::Grid;
+use crossbeam::channel::{Receiver, Sender};
 
 pub mod math;
 pub mod mesh_to_grid;
@@ -20,106 +21,173 @@ mod aimbot;
 
 const MESH_FILE: &str = "../run-data/mesh.ply";
 
-pub struct AutonomousState {
-    pub speed: MotorSpeeds,
+/// Commands for modifying the autonomous behavior.
+pub enum Command {
+    SetSpeed(MotorSpeeds),
     /// The theta component should be in degrees.
-    pub target_location: Option<Location>,
-    pub path: Option<Path>,
-    pub grid: Grid,
-    pub zed_imu_data: ZedImuData,
+    SetTarget(Option<Location>),
+    EndAutonomous
 }
 
-pub fn zhu_li_do_the_thing(vision: &mut MonkeyVision, params: &AutonomousParams, state: &mut AutonomousState, dt: Duration) -> MotorSpeeds {
-    if let Err(err) = vision.imu_data(&mut state.zed_imu_data) {
-        error!("{:?}", err);
+struct AutonomousState {
+    speed: MotorSpeeds,
+    target: Option<Location>,
+    keep_going: bool,
+    imu_data: ZedImuData,
+    path: Option<Path>,
+    grid: Grid,
+    time_since_last_spatial_map_update: Duration,
+    last_time: Instant
+}
+
+impl AutonomousState {
+    fn apply_command(&mut self, command: Command) {
+        match command {
+            Command::SetSpeed(speed) => self.speed = speed,
+            Command::SetTarget(target) => self.target = target,
+            Command::EndAutonomous => {
+                self.path = None;
+                self.target = None;
+                self.keep_going = false;
+            }
+        }
+    }
+}
+
+/// Channels for communicating with the autonomous mode controller.
+pub struct ZhuLi {
+    pub imu_data_rec: Receiver<ZedImuData>,
+    pub command_rec: Receiver<Command>,
+    pub speed_send: Sender<MotorSpeeds>
+}
+
+impl ZhuLi {
+    /// Starts the autonomous control loop in a newly spawned thread.
+    pub fn do_the_thing(&mut self, params: &AutonomousParams) {
+        let join_handle = thread::spawn(|| {
+            let mut state = AutonomousState {
+                speed: Default::default(),
+                target: None,
+                keep_going: true,
+                imu_data: Default::default(),
+                path: None,
+                grid: Grid::new(params.min_x, params.max_x, params.min_z, params.max_z, params.res_x, params.res_z),
+                time_since_last_spatial_map_update: Duration::from_secs(0),
+                last_time: Instant::now()
+            };
+
+            while state.keep_going {
+                let now = Instant::now();
+                let dt = now.duration_since(last_time);
+                state.last_time = now;
+
+                state.time_since_last_spatial_map_update += dt;
+                if state.time_since_last_spatial_map_update >= params.min_mesh_to_grid_period {
+                    match mesh_to_grid::mesh_to_grid(&mut state.grid, MESH_FILE, params.interaction_radius, params.vertical_cutoff) {
+                        Ok(_) => {
+                            // TODO: If path is some, check it against the new grid to ensure that it is still safe. If not safe, clear path.
+                            //  Currently just clearing the path regardless
+                            state.path = None;
+                        }
+                        Err(err) => error!("{:?}", err)
+                    }
+                    state.time_since_last_spatial_map_update = Duration::from_secs(0);
+                }
+
+                match self.imu_data_rec.try_recv() {
+                    Ok(ok) => state.imu_data = ok,
+                    Err(err) => error!("{:?}", err)
+                }
+
+                let new_speeds = ZhuLi::the_thing(&mut state, params, dt);
+                if let Err(err) = self.speed_send.try_send(new_speeds) {
+                    error!("{:?}", err);
+                }
+
+                while !self.command_rec.is_empty() {
+                    state.apply_command(self.command_rec.try_recv().unwrap());
+                }
+            }
+        });
+
+        if let Err(err) = join_handle.join() {
+            panic::resume_unwind(err);
+        }
     }
 
-    let left_lin_speed = state.speed.left_speed * params.wheel_radius;
-    let right_lin_speed = state.speed.right_speed * params.wheel_radius;
+    fn the_thing(state: &mut AutonomousState, params: &AutonomousParams, dt: Duration) -> MotorSpeeds {
+        let left_lin_speed = state.speed.left_speed * params.wheel_radius;
+        let right_lin_speed = state.speed.right_speed * params.wheel_radius;
 
-    let vehicle = Vehicle {
-        pos: Vec2D { x: state.zed_imu_data.x_pos, y: state.zed_imu_data.z_pos },
-        speed: (left_lin_speed + right_lin_speed) / 2.0,
-        orientation: state.zed_imu_data.z_rot.to_radians(),
-        max_speed: params.max_speed,
-        max_force: params.max_force,
-        mass: params.mass,
-        moment_of_inertia: params.moment_of_inertia,
-        allow_backwards: params.allow_backwards,
-    };
+        let vehicle = Vehicle {
+            pos: Vec2D { x: state.imu_data.x_pos, y: state.imu_data.z_pos },
+            speed: (left_lin_speed + right_lin_speed) / 2.0,
+            orientation: state.imu_data.z_rot.to_radians(),
+            max_speed: params.max_speed,
+            max_force: params.max_force,
+            mass: params.mass,
+            moment_of_inertia: params.moment_of_inertia,
+            allow_backwards: params.allow_backwards,
+        };
 
-    let steering = match state.target_location {
-        Some(target_location) => {
-            if state.path.is_none() {
-                let s = MonkeyStateSpace {
-                    cost: state.grid.clone(),
-                    model: MonkeyModel {
-                        length: state.grid.res_x(),
-                        width: state.grid.res_z(),
-                        ang_res: 64,
-                        min_turn_rad: (params.min_turn_radius / state.grid.x_scale()).ceil() as u16, // TODO: x_scale vs z_scale?
-                        max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
-                        rev_max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
-                    },
-                };
+        let steering = match state.target.as_ref() {
+            Some(target) => {
+                if path.is_none() {
+                    let s = MonkeyStateSpace {
+                        cost: state.grid.clone(),
+                        model: MonkeyModel {
+                            length: state.grid.res_x(),
+                            width: state.grid.res_z(),
+                            ang_res: 64,
+                            min_turn_rad: (params.min_turn_radius / state.grid.x_scale()).ceil() as u16, // TODO: x_scale vs z_scale?
+                            max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
+                            rev_max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
+                        },
+                    };
 
-                let mut astar = AStar::new(s);
+                    let mut astar = AStar::new(s);
 
-                let path = astar.find_path(&DiscreteState {
-                    position: RobotVector {
-                        x: (state.zed_imu_data.x_pos / state.grid.x_scale()).floor() as i16,
-                        y: (state.zed_imu_data.z_pos / state.grid.z_scale()).floor() as i16,
-                        r: (state.zed_imu_data.z_rot * 64.0 / 360.0).floor() as i16,
-                    }
-                }, &DiscreteState {
-                    position: RobotVector {
-                        x: (target_location.x / state.grid.x_scale()).floor() as i16,
-                        y: (target_location.y / state.grid.z_scale()).floor() as i16,
-                        r: (target_location.theta * 64.0 / 360.0).floor() as i16,
-                    }
-                });
-
-                state.path = path.map(|path| -> Path {
-                    let waypoints = path.iter().map(|d| -> Vec2D {
-                        Vec2D {
-                            x: d.position.x as f32 * state.grid.x_scale(),
-                            y: d.position.y as f32 * state.grid.z_scale(),
+                    let discrete_path = astar.find_path(&DiscreteState {
+                        position: RobotVector {
+                            x: (state.imu_data.x_pos / grid.x_scale()).floor() as i16,
+                            y: (state.imu_data.z_pos / grid.z_scale()).floor() as i16,
+                            r: (state.imu_data.z_rot * 64.0 / 360.0).floor() as i16,
                         }
-                    }).collect();
+                    }, &DiscreteState {
+                        position: RobotVector {
+                            x: (target.x / grid.x_scale()).floor() as i16,
+                            y: (target.y / grid.z_scale()).floor() as i16,
+                            r: (target.theta * 64.0 / 360.0).floor() as i16,
+                        }
+                    });
 
-                    Path::new(waypoints).unwrap()
-                });
+                    state.path = discrete_path.map(|path| -> Path {
+                        let waypoints = path.iter().map(|d| -> Vec2D {
+                            Vec2D {
+                                x: d.position.x as f32 * grid.x_scale(),
+                                y: d.position.y as f32 * grid.z_scale(),
+                            }
+                        }).collect();
+
+                        Path::new(waypoints).unwrap()
+                    });
+                }
+
+                match state.path.as_ref() {
+                    Some(path) => vehicle.follow(path, params.stopping_dist, state.target.theta.to_radians(), dt),
+                    None => vehicle.stop()
+                }
             }
+            None => vehicle.stop()
+        };
 
-            match state.path.as_ref() {
-                Some(path) => vehicle.follow(path, params.stopping_dist, target_location.theta.to_radians(), dt),
-                None => vehicle.stop()
-            }
-        }
-        None => vehicle.stop()
-    };
+        let accel = steering.thrust / params.mass;
+        let rot_accel = steering.torque / params.moment_of_inertia;
+        let left_accel = accel + rot_accel * params.drive_width / 2.0;
+        let right_accel = accel - rot_accel * params.drive_width / 2.0;
+        let left_speed = (left_lin_speed + left_accel * dt.as_secs_f32()) / params.wheel_radius;
+        let right_speed = (right_lin_speed + right_accel * dt.as_secs_f32()) / params.wheel_radius;
 
-    let accel = steering.thrust / params.mass;
-    let rot_accel = steering.torque / params.moment_of_inertia;
-    let left_accel = accel + rot_accel * params.drive_width / 2.0;
-    let right_accel = accel - rot_accel * params.drive_width / 2.0;
-    let left_speed = (left_lin_speed + left_accel * dt.as_secs_f32()) / params.wheel_radius;
-    let right_speed = (right_lin_speed + right_accel * dt.as_secs_f32()) / params.wheel_radius;
-
-    MotorSpeeds { right_speed, left_speed }
-}
-
-pub fn visual_processing(vision: &mut MonkeyVision, params: &AutonomousParams, state: &mut AutonomousState) {
-    // Note that there is no guarantee that the requested map update will happen before
-    // mesh_to_grid is called. It's just that both the request and mesh_to_grid should
-    // only be ran periodically as they are computationally expensive.
-    vision.request_map_update();
-    match mesh_to_grid::mesh_to_grid(&mut state.grid, MESH_FILE, params.interaction_radius, params.vertical_cutoff) {
-        Ok(_) => {
-            // TODO: If path is some, check it against the new grid to ensure that it is still safe. If not safe, clear path.
-            //  Currently just clearing the path regardless
-            state.path = None;
-        }
-        Err(err) => error!("{:?}", err)
+        MotorSpeeds { right_speed, left_speed }
     }
 }
