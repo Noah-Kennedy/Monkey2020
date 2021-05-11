adc1d0b33ca99f3100675b09874d3964a06f08e5
noah <nomaxx117@gmail.com>
2021-05-11 14:54:06 -0500

    fixed wallaces jank

--- sources/rust/monkey-unified-command/src/command.rs
+++ sources/rust/monkey-unified-command/src/command.rs
@@ -1,6 +1,6 @@
 use crossbeam::channel::{Receiver, Sender, TrySendError, TryRecvError};
 use actix_web::web::{Data, Path, Payload, Json};
-use actix_web::{HttpResponse, ResponseError};
+use actix_web::{HttpResponse, ResponseError, web};
 use space_monkeys::Command;
 use monkey_api::{MotorSpeeds, Location};
 use std::fmt::{Display, Formatter};
@@ -19,7 +19,7 @@ impl Display for CommandError {
         match self {
             CommandError::TryRecv(err) => writeln!(f, "{:?}", err),
             CommandError::TrySend(err) => writeln!(f, "{:?}", err)
-        }
+        }?;
 
         Ok(())
     }
@@ -27,14 +27,7 @@ impl Display for CommandError {
 
 impl Error for CommandError {}
 
-impl ResponseError for CommandError {
-    fn status_code(&self) -> StatusCode {
-        match self {
-            CommandError::TryRecv(_) => StatusCode::BAD_REQUEST,
-            CommandError::TrySend(_) => StatusCode::BAD_REQUEST
-        }
-    }
-}
+impl ResponseError for CommandError {}
 
 pub struct CommandManager {
     pub command_send: Sender<Command>,
@@ -42,33 +35,35 @@ pub struct CommandManager {
 }
 
 #[actix_web::get("/get_speed")]
-pub async fn get_speed(Path(()): Path<()>, manager: Data<CommandManager>) -> Result<HttpResponse, CommandError> {
+pub async fn get_speed(
+    Path(()): Path<()>, manager: Data<CommandManager>
+) -> Result<web::Json<MotorSpeeds>, CommandError> {
     match manager.speed_rec.try_recv() {
-        Ok(speed) => Ok(HttpResponse::Ok().body(speed)),
-        Err(err) => CommandError::TryRecv(err)
+        Ok(speed) => Ok(web::Json(speed)),
+        Err(err) => Err(CommandError::TryRecv(err))
     }
 }
 
 #[actix_web::post("/command/set_speed")]
-pub async fn set_speed(speed: Json<MotorSpeeds>, manager: Data<CommandManager>) -> Result<HttpResponse, CommandError> {
+pub async fn set_speed(speed: Json<MotorSpeeds>, manager: Data<CommandManager>) -> Result<(HttpResponse), CommandError> {
     match manager.command_send.try_send(Command::SetSpeed(speed.0)) {
-        Ok(_) => Ok(HttpResponse::Ok().body({})),
-        Err(err) => CommandError::TrySend(err)
+        Ok(_) => Ok(HttpResponse::Ok().finish()),
+        Err(err) => Err(CommandError::TrySend(err))
     }
 }
 
 #[actix_web::post("/command/set_target")]
 pub async fn set_target(target: Json<Option<Location>>, manager: Data<CommandManager>) -> Result<HttpResponse, CommandError> {
     match manager.command_send.try_send(Command::SetTarget(target.0)) {
-        Ok(_) => Ok(HttpResponse::Ok().body({})),
-        Err(err) => CommandError::TrySend(err)
+        Ok(_) => Ok(HttpResponse::Ok().finish()),
+        Err(err) => Err(CommandError::TrySend(err))
     }
 }
 
 #[actix_web::post("/command/end_autonomous")]
 pub async fn end_autonomous(manager: Data<CommandManager>) -> Result<HttpResponse, CommandError> {
     match manager.command_send.try_send(Command::EndAutonomous) {
-        Ok(_) => Ok(HttpResponse::Ok().body({})),
-        Err(err) => CommandError::TrySend(err)
+        Ok(_) => Ok(HttpResponse::Ok().finish()),
+        Err(err) => Err(CommandError::TrySend(err))
     }
 }
