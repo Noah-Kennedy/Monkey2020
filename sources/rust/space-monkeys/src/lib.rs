84c7e9b4e30e70dc244efbbbc29c0e2b519e2541
Wallace Watler <watlerkw@msoe.edu>
2021-05-12 17:28:22 -0500

    Fixed my dumbfiretruckery

--- sources/rust/space-monkeys/src/lib.rs
+++ sources/rust/space-monkeys/src/lib.rs
@@ -91,7 +91,7 @@ impl ZhuLi {
 
     /// Starts the autonomous control loop. This blocks until `Command::EndAutonomous` is sent.
     pub fn do_the_thing(&mut self, vision: &mut MonkeyVision, mesh_file: &str) {
-        if let Some(state) = &mut self.state {
+        if let Some(state) = self.state.as_mut() {
             let now = Instant::now();
             let dt = now.duration_since(state.last_time);
             state.last_time = now;
@@ -113,41 +113,41 @@ impl ZhuLi {
                 error!("{:?}", err);
             }
 
-            let new_speeds = self.the_thing(dt);
+            let new_speeds = ZhuLi::the_thing(state, &self.params, dt);
             if let Err(err) = self.speed_send.send(new_speeds) {
                 error!("{:?}", err);
             }
         }
     }
 
-    fn the_thing(&mut self, dt: Duration) -> MotorSpeeds {
-        let left_lin_speed = self.state.speed.left_speed * self.params.wheel_radius;
-        let right_lin_speed = self.state.speed.right_speed * self.params.wheel_radius;
+    fn the_thing(state: &mut NavState, params: &AutonomousParams, dt: Duration) -> MotorSpeeds {
+        let left_lin_speed = state.speed.left_speed * params.wheel_radius;
+        let right_lin_speed = state.speed.right_speed * params.wheel_radius;
 
         let vehicle = Vehicle {
-            pos: Vec2D { x: self.state.imu_data.x_pos, y: self.state.imu_data.z_pos },
+            pos: Vec2D { x: state.imu_data.x_pos, y: state.imu_data.z_pos },
             speed: (left_lin_speed + right_lin_speed) / 2.0,
-            orientation: self.state.imu_data.z_rot.to_radians(),
-            max_speed: self.params.max_speed,
-            max_force: self.params.max_force,
-            mass: self.params.mass,
-            moment_of_inertia: self.params.moment_of_inertia,
-            allow_backwards: self.params.allow_backwards,
+            orientation: state.imu_data.z_rot.to_radians(),
+            max_speed: params.max_speed,
+            max_force: params.max_force,
+            mass: params.mass,
+            moment_of_inertia: params.moment_of_inertia,
+            allow_backwards: params.allow_backwards,
         };
 
-        let steering = match self.state.target.as_ref() {
+        let steering = match state.target.as_ref() {
             Some(target) => {
-                if self.state.path.is_none() {
+                if state.path.is_none() {
                     // TODO: Have path computation on a separate thread?
                     let s = MonkeyStateSpace {
-                        cost: self.state.grid.clone(),
+                        cost: state.grid.clone(),
                         model: MonkeyModel {
-                            length: self.state.grid.res_x(),
-                            width: self.state.grid.res_z(),
+                            length: state.grid.res_x(),
+                            width: state.grid.res_z(),
                             ang_res: 64,
-                            min_turn_rad: (self.params.min_turn_radius / self.state.grid.x_scale()).ceil() as u16, // TODO: x_scale vs z_scale?
-                            max_speed: (self.params.max_speed / self.state.grid.x_scale()).floor() as u16,
-                            rev_max_speed: (self.params.max_speed / self.state.grid.x_scale()).floor() as u16,
+                            min_turn_rad: (params.min_turn_radius / state.grid.x_scale()).ceil() as u16, // TODO: x_scale vs z_scale?
+                            max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
+                            rev_max_speed: (params.max_speed / state.grid.x_scale()).floor() as u16,
                         },
                     };
 
@@ -155,23 +155,23 @@ impl ZhuLi {
 
                     let discrete_path = astar.find_path(&DiscreteState {
                         position: RobotVector {
-                            x: (self.state.imu_data.x_pos / self.state.grid.x_scale()).floor() as i16,
-                            y: (self.state.imu_data.z_pos / self.state.grid.z_scale()).floor() as i16,
-                            r: (self.state.imu_data.z_rot * 64.0 / 360.0).floor() as i16,
+                            x: (state.imu_data.x_pos / state.grid.x_scale()).floor() as i16,
+                            y: (state.imu_data.z_pos / state.grid.z_scale()).floor() as i16,
+                            r: (state.imu_data.z_rot * 64.0 / 360.0).floor() as i16,
                         }
                     }, &DiscreteState {
                         position: RobotVector {
-                            x: (target.x / self.state.grid.x_scale()).floor() as i16,
-                            y: (target.y / self.state.grid.z_scale()).floor() as i16,
+                            x: (target.x / state.grid.x_scale()).floor() as i16,
+                            y: (target.y / state.grid.z_scale()).floor() as i16,
                             r: (target.theta * 64.0 / 360.0).floor() as i16,
                         }
                     });
 
-                    self.state.path = discrete_path.map(|path| -> Path {
+                    state.path = discrete_path.map(|path| -> Path {
                         let waypoints = path.iter().map(|d| -> Vec2D {
                             Vec2D {
-                                x: d.position.x as f32 * self.state.grid.x_scale(),
-                                y: d.position.y as f32 * self.state.grid.z_scale(),
+                                x: d.position.x as f32 * state.grid.x_scale(),
+                                y: d.position.y as f32 * state.grid.z_scale(),
                             }
                         }).collect();
 
@@ -179,20 +179,20 @@ impl ZhuLi {
                     });
                 }
 
-                match self.state.path.as_ref() {
-                    Some(path) => vehicle.follow(path, self.params.stopping_dist, target.theta.to_radians(), dt),
+                match state.path.as_ref() {
+                    Some(path) => vehicle.follow(path, params.stopping_dist, target.theta.to_radians(), dt),
                     None => vehicle.stop()
                 }
             }
             None => vehicle.stop()
         };
 
-        let accel = steering.thrust / self.params.mass;
-        let rot_accel = steering.torque / self.params.moment_of_inertia;
-        let left_accel = accel + rot_accel * self.params.drive_width / 2.0;
-        let right_accel = accel - rot_accel * self.params.drive_width / 2.0;
-        let left_speed = (left_lin_speed + left_accel * dt.as_secs_f32()) / self.params.wheel_radius;
-        let right_speed = (right_lin_speed + right_accel * dt.as_secs_f32()) / self.params.wheel_radius;
+        let accel = steering.thrust / params.mass;
+        let rot_accel = steering.torque / params.moment_of_inertia;
+        let left_accel = accel + rot_accel * params.drive_width / 2.0;
+        let right_accel = accel - rot_accel * params.drive_width / 2.0;
+        let left_speed = (left_lin_speed + left_accel * dt.as_secs_f32()) / params.wheel_radius;
+        let right_speed = (right_lin_speed + right_accel * dt.as_secs_f32()) / params.wheel_radius;
 
         MotorSpeeds { right_speed, left_speed }
     }
