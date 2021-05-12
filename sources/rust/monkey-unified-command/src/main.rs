8a0bd19d329310f57c9f59729c431f0d31c6f82e
noah <nomaxx117@gmail.com>
2021-05-11 20:14:08 -0500

    added hookers

--- sources/rust/monkey-unified-command/src/main.rs
+++ sources/rust/monkey-unified-command/src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{App, HttpServer};
+use cameralot::prelude::*;
+use std::fs;
 
 mod util;
 mod camera;
@@ -6,7 +8,10 @@ mod command;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut instagram_hoe = OpenCVCameraFeed::new();
+    instagram_hoe.open(0);
+
+    let server = HttpServer::new(|| {
         App::new()
             .service(camera::static_image)
             .service(camera::ws_camera)
@@ -16,6 +21,34 @@ async fn main() -> std::io::Result<()> {
             .service(command::end_autonomous)
     })
         .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+        .run();
+
+    let mut td = TimerData {
+        grab_millis: 0,
+        retrieve_millis: 0,
+        resize_millis: 0,
+        encode_millis: 0
+    };
+
+    for i in 0..1000 {
+        let path = format!("images/{}{}", i, FILE_FORMAT);
+
+        let timer = std::time::Instant::now();
+
+        let buf = unsafe {
+            instagram_hoe.read(WIDTH, HEIGHT, FILE_FORMAT, &mut td).unwrap()
+        };
+
+        let time = timer.elapsed().as_millis();
+
+        println!("{}:\t{} millis", i, time);
+        println!("\tGrab: {} millis", td.grab_millis);
+        println!("\tRetrieve: {} millis", td.retrieve_millis);
+        println!("\tResize: {} millis", td.resize_millis);
+        println!("\tEncode: {} millis", td.encode_millis);
+
+        fs::write(&path, buf).unwrap();
+    }
+
+    server.await
 }
